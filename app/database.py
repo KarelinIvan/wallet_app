a7703a6bc7712ca3b8bb083870961ab82d6d791b
KarelinIvan <ivan.karelin.1993@mail.ru>
2025-02-04 13:13:09 +0300

    2-й коммит

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://username:password@hostname:port/database_name"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Mt076954@127.0.0.1:5432/apitest"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
